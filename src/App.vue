06a7cbc251b3df6728f50b76531734b9459a57d9
yaasir cheekoory <yaasir1997@gmail.com>
2023-10-08 21:43:55 +0400

    wip about section

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import Particles from './components/Particles.vue'
 import Nav from './components/Nav.vue'
 import Intro from "./components/Intro.vue";
+import About from './components/About.vue'
 </script>
 
 <template>
   <Particles />
   <Nav />
   <Intro />
+  <About />
 </template>
 
---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="about-section">
+    <div class="about-content">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ea itaque, dolores qui doloremque commodi nihil ipsum eligendi, maxime illo perspiciatis id quae sunt odio. Exercitationem, rem? Ullam, blanditiis odit.
+    </div>
+    <div class="about-img"></div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
