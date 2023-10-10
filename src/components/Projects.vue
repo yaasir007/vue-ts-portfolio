a8b8a876f7c6382706d45474c2da1d6b4f28c257
yaasir cheekoory <yaasir1997@gmail.com>
2023-10-10 23:21:18 +0400

    added parkium img

---
+++ src/components/Projects.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="projects-section">
+    <div class="projects-title">Projects</div>
+    <div class="projects-container">
+      <div class="project">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.projects-section {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
