a350d77f7098dda88ad5522253808236cfd3d955
yaasir cheekoory <yaasir1997@gmail.com>
2023-10-15 00:38:07 +0400

    wip

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -14,8 +14,8 @@ import Parkium from "../assets/projects/parkium.jpeg";
           <span class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
             magni ut minima earum quasi ad?</span>
             <div class="project-btns">
-              <button>Open Website</button>
-              <button>Open Github</button>
+              <button>Website</button>
+              <button>Github</button>
             </div>
         </div>
       </div>
@@ -28,8 +28,8 @@ import Parkium from "../assets/projects/parkium.jpeg";
           <span class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
             magni ut minima earum quasi ad?</span>
             <div class="project-btns">
-              <button>Open Website</button>
-              <button>Open Github</button>
+              <button>Website</button>
+              <button>Github</button>
             </div>
         </div>
       </div>
@@ -42,8 +42,8 @@ import Parkium from "../assets/projects/parkium.jpeg";
           <span class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
             magni ut minima earum quasi ad?</span>
             <div class="project-btns">
-              <button>Open Website</button>
-              <button>Open Github</button>
+              <button>Website</button>
+              <button>Github</button>
             </div>
         </div>
       </div>
@@ -56,8 +56,8 @@ import Parkium from "../assets/projects/parkium.jpeg";
           <span class="project-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
             magni ut minima earum quasi ad?</span>
             <div class="project-btns">
-              <button>Open Website</button>
-              <button>Open Github</button>
+              <button>Website</button>
+              <button>Github</button>
             </div>
         </div>
       </div>
@@ -114,5 +114,51 @@ import Parkium from "../assets/projects/parkium.jpeg";
   }
 }
 
+.project-img {
+  z-index: 999;
+}
+
+button {
+  align-items: center;
+  appearance: none;
+  background-color: #FCFCFD;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #36395A;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono",monospace;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding: 1rem;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 12px;
+}
+
+button:focus {
+  box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+}
+
+button:hover {
+  box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+button:active {
+  box-shadow: #D6D6E7 0 3px 7px inset;
+  transform: translateY(2px);
+}
 
 </style>
