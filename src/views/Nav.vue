f959edb41e0404781ef2620279d0dad6d5cf543c
Mohammad Yaasir Cheekoory <77941119+yaasir007@users.noreply.github.com>
2024-02-16 09:54:51 +0400

    Revert "Mobile nav"

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
-import MenuBar from '../assets/socials/menu.png'
 import Logo from '../assets/myc-logo.png'
-
-const drawer = ref(false)
 </script>
 
 <template>
@@ -24,51 +20,18 @@ const drawer = ref(false)
       <RouterLink active-class="active" class="nav-link" to="/tools">&lt;Tools&gt;</RouterLink>
     </div>
   </div>
-
-  <div class="nav-section-mobile">
-    <v-card>
-      <v-layout>
-        <v-app-bar
-          color="transparent"
-          prominent
-        >
-          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer">
-            <img :src="MenuBar" alt="Menu Bar" style="width: 40px;">
-          </v-app-bar-nav-icon>
-
-          <v-toolbar-title style="color: white;">MYC</v-toolbar-title>
-        </v-app-bar>
-
-        <v-navigation-drawer
-          v-model="drawer"
-          location="bottom"
-          temporary
-          style="background-color: rgb(0, 0, 0); height: 100dvh;"
-        >
-          <v-list>
-            <div class="route-links">
-              <RouterLink active-class="active" class="route-link" to="/">Home</RouterLink>
-              <RouterLink active-class="active" class="route-link" to="/about">About</RouterLink>
-              <RouterLink active-class="active" class="route-link" to="/works">Experience</RouterLink>
-              <RouterLink active-class="active" class="route-link" to="/projects">Projects</RouterLink>
-              <RouterLink active-class="active" class="route-link" to="/blogs">Blogs</RouterLink>
-              <RouterLink active-class="active" class="route-link" to="/tools">Tools</RouterLink>
-            </div>
-          </v-list>
-        </v-navigation-drawer>
-      </v-layout>
-    </v-card>
-  </div>
 </template>
 
 <style lang="scss" scoped>
 .nav-section {
+  z-index: 99999;
   width: 100%;
   position: fixed;
   top: 0;
   height: 8vh;
   background-color: #373f7a;
   padding: .8rem;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -93,8 +56,10 @@ const drawer = ref(false)
     .nav-link {
       text-decoration: none;
       font-size: clamp(0.3rem, 4vw, 1rem);
+
       transition: all .2s ease-in;
       color: white;
+      transition: all .2s linear;
       opacity: 0;
     }
 
@@ -127,41 +92,6 @@ const drawer = ref(false)
     .nav-links {
       display: none;
     }
-  }
-}
-
-.nav-section-mobile {
-  display: none;
-}
-
-@media screen and (max-width: 750px) {
-  .nav-section {
-    display: none;
-  }
-  .nav-section-mobile {
-    display: block;
-    z-index: 9999;
-  }
-
-  .route-links {
-    height: 90vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 3rem;
-    margin-block: 1rem;
-    margin-inline: 1.5rem;
-  }
-
-  .route-link {
-    font-size: 1.5rem;
-    color: white;
-    text-decoration: none;
-  }
-
-  .active {
-    display: none;
   }
 }
 </style>
