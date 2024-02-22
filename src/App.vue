cc618746b33807a9ec8a161e5494fdabf9cd2ba6
yaasir cheekoory <yaasir1997@gmail.com>
2024-02-22 23:56:27 +0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { AtomSpinner } from 'epic-spinners'
+
 import { RouterView } from 'vue-router'
 import Particles from './components/Particles.vue'
 import Nav from '../src/views/Nav.vue'
@@ -7,5 +9,6 @@ import Nav from '../src/views/Nav.vue'
 <template>
   <Particles />
   <Nav />
+  <atom-spinner :animation-duration="1000" :size="60" color="#ff1d5e" />
   <RouterView />
 </template>
