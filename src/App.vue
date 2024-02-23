91346bdb49fcf3adf90a70cbdab5b69f0e3ee932
yaasir cheekoory <yaasir1997@gmail.com>
2024-02-23 22:34:54 +0400

    made preloader a component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { AtomSpinner } from 'epic-spinners'
 
 import { RouterView } from 'vue-router'
 import Particles from './components/Particles.vue'
 import Nav from '../src/views/Nav.vue'
+import Preloader from '../src/components/Preloader.vue'
 
 const isLoading = ref(true)
 
@@ -16,21 +16,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="isLoading" class="preloader">
-    <atom-spinner :animation-duration="1500" :size="100" color="#a60080" />
-  </div>
+  <Preloader v-if="isLoading" />
   <Particles v-if="!isLoading" />
   <Nav v-if="!isLoading" />
   <RouterView v-if="!isLoading" />
 </template>
 
 <style scoped lang="scss">
-.preloader {
-  background-color: black;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 </style>
---
+++ src/components/Preloader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { AtomSpinner } from 'epic-spinners'
+</script>
+
+<template>
+  <div class="preloader">
+    <atom-spinner :animation-duration="1500" :size="100" color="#a60080" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.preloader {
+  background-color: black;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
