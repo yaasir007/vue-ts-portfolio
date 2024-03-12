4ca750c5d0132d16d723a81824b450702f72d519
yaasir cheekoory <yaasir1997@gmail.com>
2024-03-13 00:03:42 +0400

    installed vuetify

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,19 @@ import './style.scss'
 import App from './App.vue'
 import router from './router/index'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
+app.use(vuetify)
 app.use(ScrollReveal)
 app.use(Particles)
 app.use(router)
